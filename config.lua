513e259ed1b3f05252228b134583368cfb151dbf
Taylor McGaw <28567929+Taylor-McGaw-Skyline-DEV@users.noreply.github.com>
2024-11-08 11:20:17 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,36 @@
+Config= {}
+
+Config.Locale = 'en'
+
+Config.Delay = 5000
+
+Config.FireJob = 'fire'
+
+Config.JobCheck = true
+
+Config.EX = {
+    -666581633,
+    -1980225301,
+    -1610165324,
+    -875057463,
+    -171327159
+}
+
+Translation = {
+    ['en'] = {
+        ['error1'] = 'Error',
+        ['error2'] = 'You already have a fire extinguisher!',
+        ['error3'] = "You don't have a fire extinguisher in your inventory!",
+        ['success1'] = 'Success',
+        ['success2'] = 'Fire extinguisher received!',
+        ['success3'] = 'Your fire extinguisher has been replaced!',
+        ['inform1'] = 'Info',
+        ['inform2'] = "You don't have a fire extinguisher in your inventory!",
+        ['target_1'] = 'Take / Replace Extinguisher',
+        ['menu_header'] = 'Fire Extinguisher',
+        ['menu_1'] = 'Take Extinguisher',
+        ['menu_2'] = 'Replace Extinguisher',
+        ['remfire'] = 'Put Away Extinguisher',
+        ['remfire1'] = 'You have put away your fire extinguisher',
+    }
+}
